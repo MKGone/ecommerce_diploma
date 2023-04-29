dda6082dedb240958cc12d65b0e9b02ff551c886
MKGone <mixal7739@gmail.com>
2023-04-29 12:48:49 +0300

    Initial Commit

---
+++ ecommerce_test_app/ecommerce_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerce_test_appApp.swift
+//  ecommerce_test_app
+//
+//  Created by Mykhailo Kovalskyi on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ecommerce_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
